b07a9d73ad85fbdb8d24cfd466838892841c86bb
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-09-22 22:35:21 +0300

    Edited Delete Form

--- client/src/components/InputForm.jsx
+++ client/src/components/InputForm.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { AiFillDelete } from "react-icons/ai";
 
@@ -31,12 +30,13 @@ const InputForm = () => {
 
   const [dataVisible, setDataVisible] = useState(false); // Track visibility state
   const [errorMessage, setErrorMessage] = useState("");
-
-  const [firstNameError, setFirstNameError] = useState(false);
-  const [lastNameError, setLastNameError] = useState(false);
-  const [timeError, setTimeError] = useState(false);
+  const [responseMessage, setResponseMessage] = useState("");
 
   const [responseData, setResponseData] = useState([]); // Get response
+
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [recordToDelete, setRecordToDelete] = useState(null);
+  const [recordIndexToDelete, setRecordIndexToDelete] = useState(null);
 
   const handleFirstNameChange = (e) => {
     setFirstName(e.target.value);
@@ -50,25 +50,20 @@ const InputForm = () => {
     setTime(e.target.value);
   };
 
+  const clearForm = () => {
+    setFirstName("");
+    setLastName("");
+    setTime("");
+  };
+
   const handlePostRequest = () => {
     // Clear any previous error messages and reset error states
     setErrorMessage("");
-    setFirstNameError(false);
-    setLastNameError(false);
-    setTimeError(false);
+    setResponseMessage("");
 
     // Check if any of the required fields are empty
     if (!firstName || !lastName || !time) {
       setErrorMessage("Please fill in all required fields.");
-      if (!firstName) {
-        setFirstNameError(true);
-      }
-      if (!lastName) {
-        setLastNameError(true);
-      }
-      if (!time) {
-        setTimeError(true);
-      }
       return; // Exit the function if validation fails
     }
 
@@ -86,19 +81,14 @@ const InputForm = () => {
     axios
       .post(apiUrl, postData)
       .then(function (response) {
-        // Handle the response data here
-        console.log("Response:", response.data);
+        // console.log("Response:", response.data);
 
-        // Clear the form by resetting the input values
-        setFirstName("");
-        setLastName("");
-        setTime("");
-
-        // Trigger a GET request to refresh the data
+        clearForm();
+        setResponseMessage("New Recrod Added.");
         handleGetRequest();
       })
       .catch(function (error) {
-        console.error("Ops:", error.response.data.message);
+        // console.error("Ops:", error.response.data.message);
         // Handle any errors from the server
       });
   };
@@ -116,20 +106,42 @@ const InputForm = () => {
         // console.log("Response:", response.data);
       })
       .catch(function (error) {
-        console.error("Ops:", error);
+        // console.error("Ops:", error);
       });
   };
 
-  const handleDelete = (id) => {
+  const confirmDelete = (id, index) => {
+    // Open the delete confirmation dialog
+    setShowDeleteConfirmation(true);
+    // Set the record to delete
+    setRecordToDelete(id);
+    setRecordIndexToDelete(index);
+  };
+
+  const cancelDelete = () => {
+    // Close the delete confirmation dialog and reset the record to delete
+    setShowDeleteConfirmation(false);
+    setRecordToDelete(null);
+    setRecordIndexToDelete(null);
+  };
+
+  const handleDelete = () => {
+    setShowDeleteConfirmation(false);
+
     // Define the API endpoint URL
-    const apiUrl = "http://localhost:4001/request/delete/" + id;
+    const apiUrl = "http://localhost:4001/request/delete/" + recordToDelete;
 
     // Make the POST request using Axios
     axios
       .delete(apiUrl)
-      .then(handleGetRequest)
+      .then(function (response) {
+        console.log("Response:", response.data);
+
+        // Trigger a GET request to refresh the data
+        handleGetRequest();
+      })
       .catch(function (error) {
-        console.error("Ops:", error);
+        //  console.error("Ops:", error);
       });
   };
 
@@ -146,7 +158,7 @@ const InputForm = () => {
             value={firstName}
             onChange={handleFirstNameChange}
             className={`bg-teal-50 duration-200 rounded-xl px-4 py-2 font-bold w-100 ml-auto ${
-              firstNameError ? "border-2 border-red-500" : ""
+              errorMessage && !firstName ? "border-2 border-red-500" : ""
             }`}
           ></input>
         </div>
@@ -160,7 +172,7 @@ const InputForm = () => {
             value={lastName}
             onChange={handleLastNameChange}
             className={`bg-teal-50 duration-200 rounded-xl px-4 py-2 font-bold w-100 ml-auto ${
-              lastNameError ? "border-2 border-red-500" : ""
+              errorMessage && !lastName ? "border-2 border-red-500" : ""
             }`}
           ></input>
         </div>
@@ -173,7 +185,7 @@ const InputForm = () => {
             value={time}
             onChange={handleTimeChange}
             className={`bg-teal-50 duration-200 rounded-xl px-4 py-2 font-bold w-100 ml-auto ${
-              timeError ? "border-2 border-red-500" : ""
+              errorMessage && !time ? "border-2 border-red-500" : ""
             }`}
           >
             <optgroup label="Select Time">
@@ -184,15 +196,18 @@ const InputForm = () => {
           </select>
         </div>
 
-        {/* Display the error message */}
-        {errorMessage && (
+        {/* Display the message */}
+        {(errorMessage && (
           <div className="text-red-500 mb-2">{errorMessage}</div>
-        )}
+        )) ||
+          (responseMessage && (
+            <div className="text-green-500 mb-2">{responseMessage}</div>
+          ))}
 
         <div className="flex">
           <button
             onClick={handlePostRequest}
-            className="mx-auto hover:bg-teal-300 bg-teal-50 duration-200 rounded-xl px-4 py-2 font-bold border-0"
+            className="mx-auto hover:bg-teal-300 hover:scale-105 bg-teal-50 duration-200 rounded-xl px-4 py-2 font-bold border-0"
           >
             Submit
           </button>
@@ -208,20 +223,24 @@ const InputForm = () => {
               if (!dataVisible) handleGetRequest();
 
               // Toggle data visibility when the button is clicked
-              setDataVisible(!dataVisible);
+              if (!recordToDelete) setDataVisible(!dataVisible);
             }}
-            className="mx-auto hover:bg-teal-300 bg-teal-50 duration-200 rounded-xl px-4 py-2 mt-2 font-bold border-0"
+            className={`mx-auto bg-teal-50 duration-200 rounded-xl px-4 py-2 mt-2 font-bold border-0 ${
+              !recordToDelete ? "hover:bg-teal-300 hover:scale-105" : ""
+            }`}
           >
             Get Data
           </button>
         </div>
         <div className="mt-2">
-          {dataVisible ? (
+          {dataVisible &&
             responseData.map((item, index) => (
               <div key={index} className="flex">
                 <button
-                  onClick={() => handleDelete(item._id)} // Pass a callback function here
-                  className="hover:text-red-700 hover:scale-105 duration-200 mr-2"
+                  onClick={() => confirmDelete(item._id, index + 1)} // Pass a callback function here
+                  className={`hover:text-red-700 hover:scale-105 duration-200 mr-2 ${
+                    recordToDelete === item._id ? " text-red-700 scale-105" : ""
+                  }`}
                 >
                   <AiFillDelete />
                 </button>
@@ -229,11 +248,27 @@ const InputForm = () => {
                   {index + 1}- {item.firstName} {item.lastName} - {item.time}
                 </p>
               </div>
-            ))
-          ) : (
-            <div></div>
-          )}
+            ))}
         </div>
+        {showDeleteConfirmation && (
+          <div className="bg-white p-4 rounded-md shadow-md mt-2 duration-150">
+            <p>Are you sure you want to delete record {recordIndexToDelete}?</p>
+            <div className="mt-4 flex justify-end">
+              <button
+                onClick={handleDelete}
+                className="bg-red-500 hover:scale-105 duration-200 text-white px-4 py-2 rounded-md mr-2"
+              >
+                Confirm
+              </button>
+              <button
+                onClick={cancelDelete}
+                className="bg-slate-200 hover:bg-teal-300 hover:scale-105 duration-200 px-4 py-2 rounded-md"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
